d867fb4585c8531c4284c1c5f47f13ec70a5502b
francisco-ca <francisco4_1996@hotmail.com>
2020-12-04 01:07:51 +0000

    ListaEsperaavanzada

--- src/js/components/NavbarTop.js
+++ src/js/components/NavbarTop.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom'
 import {Navbar, Nav} from 'react-bootstrap'
 
 const NavbarTop = () => {
+    // let pers = sessionStorage.getItem('persona_data')
+    // let perso= JSON.parse(pers)
+    // let person= perso.usuario
     return (
         <Navbar bg="light" variant="light" expand="lg">
             <Navbar.Brand>
@@ -14,7 +17,7 @@ const NavbarTop = () => {
                         alt='brand'
                     />   
                 </Link>
-                <span>Bienvenido</span> 
+    <span>Bienvenido/a {/* person.nombre*/}</span> 
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
